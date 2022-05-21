9773b916648299ce2874d276eb99016056509ad1
Sweta Maharjan <60056050+itssss-sweta@users.noreply.github.com>
2022-05-21 21:25:55 +0545

    Create scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -6,23 +6,23 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.hideturtle()
-        self.left_score=0
-        self.right_score=0
+        self.l_score=0
+        self.r_score=0
         self.update_score()
 
     def update_score(self):
         self.clear()
         self.goto(-100,200)
-        self.write(self.l_score,align="center",font=("Courier",80,"bold"))
+        self.write(self.left_score,align="center",font=("Courier",80,"bold"))
         self.goto(100,200)
-        self.write(self.r_score,align="center",font=("Courier",80,"bold"))
+        self.write(self.right_score,align="center",font=("Courier",80,"bold"))
     
-    def left_score(self):
-        self.left_score+=1
+    def l_score(self):
+        self.l_score+=1
         self.update_score()
 
-    def right_score(self):
-        self.right_score+=1
+    def r_score(self):
+        self.r_score+=1
         self.update_score()
 
    
